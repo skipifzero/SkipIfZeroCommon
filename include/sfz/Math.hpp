5e67f07ba661a87e191433bd5e544cc63a3510f3
Peter Hillerström <peter@hstroem.se>
2014-08-08 23:53:43 +0200

    Updates multi include headers.

--- include/sfz/Math.hpp
+++ include/sfz/Math.hpp
@@ -2,7 +2,10 @@
 #ifndef SFZ_MATH_HPP
 #define SFZ_MATH_HPP
 
+#include "sfz/math/Alignment.hpp"
+#include "sfz/math/Circle.hpp"
 #include "sfz/math/MathConstants.hpp"
+#include "sfz/math/Rectangle.hpp"
 #include "sfz/math/Vector.hpp"
 
 #endif
