716c5c32f0cd205a3c0e87959624ffc70be59227
Peter Hillerström <peter@hstroem.se>
2014-10-30 10:39:20 +0100

    Added untested exceptions enabled check for GCC.

--- include/sfz/util/ConditionalThrow.hpp
+++ include/sfz/util/ConditionalThrow.hpp
@@ -5,8 +5,21 @@
 #include <exception> // std::terminate
 
 #ifndef SFZ_EXCEPTIONS_ENABLED
-// Wip, here we will check compiler specific macros.
-#define SFZ_EXCEPTIONS_ENABLED true
+
+	// GCC
+	#ifdef __GNUG__
+		// GCC: '__EXCEPTIONS' This macro is defined, with value 1, when compiling a C++ source file with exceptions
+		// enabled. If -fno-exceptions was used when compiling the file, then this macro will not be defined. 
+		#ifdef __EXCEPTIONS
+		#define SFZ_EXCEPTIONS_ENABLED true
+		#else
+		#define SFZ_EXCEPTIONS_ENABLED false
+		#endif
+	#endif
+
+	
+
+
 #endif
 
 template<typename ExceptionType>
@@ -14,8 +27,10 @@ void sfz_throw(std::string message)
 {
 	#if SFZ_EXCEPTIONS_ENABLED == true
 		throw ExceptionType{message};
-	#else
+	#elif SFZ_EXCEPTIONS_ENABLED == false
 		std::cout << message << std::endl;
 		std::terminate();
+	#else
+	#error SFZ_EXCEPTIONS_ENABLED may only be defined as true or false
 	#endif
 }
