6d1d5a23f9d24df1de8871a5cb7a49706aa4386a
Peter Hillerström <peter@hstroem.se>
2014-11-29 17:09:46 +0100

    Math: Removed Alignment, Circle & Rectangle. The implementation wasn't particularly good.

--- include/sfz/Math.hpp
+++ include/sfz/Math.hpp
@@ -2,11 +2,8 @@
 #ifndef SFZ_MATH_HPP
 #define SFZ_MATH_HPP
 
-#include "sfz/math/Alignment.hpp"
-#include "sfz/math/Circle.hpp"
 #include "sfz/math/Matrix.hpp"
 #include "sfz/math/MathConstants.hpp"
-#include "sfz/math/Rectangle.hpp"
 #include "sfz/math/Vector.hpp"
 #include "sfz/math/VectorSupport.hpp"
 
