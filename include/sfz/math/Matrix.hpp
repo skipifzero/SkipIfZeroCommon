6de73b869a552b9640fb1a34228986ec5f6a0278
Peter Hillerström <peter.hillerstrom93@gmail.com>
2015-01-02 23:06:30 +0100

    Matrix: Setter was accidentally marked const.

--- include/sfz/math/Matrix.hpp
+++ include/sfz/math/Matrix.hpp
@@ -102,7 +102,7 @@ struct Matrix final {
 	 * @param j the j:th column
 	 * @param value the value to assign
 	 */
-	void set(size_t i, size_t j, T value) const noexcept;
+	void set(size_t i, size_t j, T value) noexcept;
 
 	/**
 	 * @brief Assigns each element in the matrix with the specified value.
