5e889b25a2f711a9e7b72d2b43f8251c7df3214b
Peter Hillerström <peter@hstroem.se>
2015-02-18 09:55:04 +0100

    IntersectionTests: Added very simple test to make sure point inside AABB functions are at least somewhat correct.

--- test/sfz/geometry/Intersection_Tests.cpp
+++ test/sfz/geometry/Intersection_Tests.cpp
@@ -4,6 +4,16 @@
 
 #include "sfz/Geometry.hpp"
 
+TEST_CASE("Point inside AABB test", "[sfz::Intersection]")
+{
+	using namespace sfz;
+
+	AABB box{vec3f{-1,-1,-1}, vec3f{1,1,1}};
+
+	REQUIRE(inside(box, vec3f{0,0,0}));
+	REQUIRE(!inside(box, vec3f{-2,0,0}));
+}
+
 TEST_CASE("AABB & AABB test", "[sfz::Intersection]")
 {
 	using namespace sfz;
