ed6b9d98427b55c1c3415518d40b5e1c95857d50
Peter Hillerström <peter@hstroem.se>
2015-07-13 22:49:23 +0200

    Squashed 'externals/SkipIfZeroCommon/' changes from 0fc1ee8..9974652
    9974652 PushWarnings.hpp: Fixed for clang.
    
    git-subtree-dir: externals/SkipIfZeroCommon
    git-subtree-split: 9974652708df67310fc6af969caa4e6381148369

--- include/sfz/PushWarnings.hpp
+++ include/sfz/PushWarnings.hpp
@@ -14,7 +14,9 @@
 
 #elif defined(__clang__)
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Weverything"
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wpedantic"
 
 #elif defined(__GNUC__)
 #pragma GCC diagnostic push
