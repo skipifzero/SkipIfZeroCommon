8963003a81f091d95e2f9c648bc85c3012fc59ec
Peter Hillerström <peter@hstroem.se>
2015-10-14 21:46:00 +0200

    Program: Simplified the process for creating post process shaders.

--- include/sfz/gl/PostProcessQuad.hpp
+++ include/sfz/gl/PostProcessQuad.hpp
@@ -21,7 +21,9 @@ public:
 private:
 	uint32_t mVAO = 0;
 	uint32_t mPosBuffer = 0;
+	uint32_t mNormalBuffer = 0;
 	uint32_t mUVBuffer = 0;
+	uint32_t mMaterialIDBuffer = 0;
 	uint32_t mIndexBuffer = 0;
 };
 
